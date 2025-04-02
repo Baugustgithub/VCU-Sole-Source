05cf246b8f3e0910dfca35885db2b42ce8fc1a04
Baugustgithub <40409162+Baugustgithub@users.noreply.github.com>
2025-04-02 12:05:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -854,7 +854,7 @@ function submitForm() {
       </svg>
       Download as Text
     </button>
-    <button id="start-over" class="btn-secondary px-4 py-2 rounded-md flex items-center justify-center">
+    <button type="button" onclick="location.reload()" class="btn-secondary px-4 py-2 rounded-md flex items-center justify-center">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
       </svg>
